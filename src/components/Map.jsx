5e4bcf731f7b632071f77b42ef85fd77e9521860
Nexabug <fusionflarepath@gmail.com>
2026-06-07 09:29:55 +0530

    added features in map like auto center to the click marker and allow the multi markers with popup message for each

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,27 +1,61 @@
-import React from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import React, { useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import { useMapEvents } from "react-leaflet";
+import { CircleMarker } from "react-leaflet";
 
 export default function Map() {
-  // Center of the map [latitude, longitude]
-  const position = [1.505, -0.09];
+  const [marker, setmarker] = useState([]);
+  const [position, setpos] = useState([50, 14]);
 
   return (
     <div className="map">
-      <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
-        {/* The base layer tiles from OpenStreetMap */}
+      <MapContainer
+        center={position}
+        zoom={13}
+        scrollWheelZoom={true}
+        keyboard={true}
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        <HandleClickMap setpos={setpos} setmarker={setmarker} />
 
-        {/* A single clickable map marker */}
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {marker.map((x) => (
+          <CustomMarker x={x} key={x[0]} />
+        ))}
       </MapContainer>
     </div>
+  );
+}
+
+function HandleClickMap({ setpos, setmarker }) {
+  const map = useMapEvents({
+    click(e) {
+      setpos(e.latlng);
+      map.flyTo(e.latlng, 11);
+      setmarker((prev) => [...prev, e.latlng]);
+    },
+  });
+
+  return null;
+}
+
+function CustomMarker({ x }) {
+  const map = useMap();
+  return (
+    <Marker
+      position={x}
+      eventHandlers={{
+        click: () => {
+          map.flyTo(x, 11);
+        },
+      }}
+    >
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
   );
 }
