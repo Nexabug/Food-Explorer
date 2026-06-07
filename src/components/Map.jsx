1fb71d23010f1e10af7c5e9e1bd00aec1ca834ac
Nexabug <fusionflarepath@gmail.com>
2026-06-07 07:59:05 +0530

    improved the side bar component and added the map from the leaflet

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
-function Map() {
+export default function Map() {
+  // Center of the map [latitude, longitude]
+  const position = [1.505, -0.09];
+
   return (
-    <div className='map'>Map</div>
-  )
-}
+    <div className="map">
+      <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
+        {/* The base layer tiles from OpenStreetMap */}
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
 
-export default Map
+        {/* A single clickable map marker */}
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+}
